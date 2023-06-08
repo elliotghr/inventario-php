6f8d5f511f62d7d615058eb56216343c8601204e
ElliotGhr <gusnroses.2617@gmail.com>
2023-06-08 17:20:12 -0600

    DB script agregada

---
+++ inventario.sql
@@ -0,0 +1,159 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-06-2023 a las 01:19:10
+-- Versión del servidor: 8.0.33
+-- Versión de PHP: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `inventario`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `categoria_id` int NOT NULL,
+  `nombre` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `ubicacion` varchar(150) COLLATE utf8mb4_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`categoria_id`, `nombre`, `ubicacion`) VALUES
+(5, 'Ropa', 'Pasillo 1'),
+(6, 'Videojuegos', 'Pasillo 2'),
+(7, 'Comida', 'Pasillo 3'),
+(8, 'Celulares', 'Pasillo 3');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `producto_id` int UNSIGNED NOT NULL,
+  `codigo` varchar(70) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `nombre` varchar(70) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `precio` decimal(30,0) NOT NULL,
+  `stock` int NOT NULL,
+  `foto` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `categoria_id` int NOT NULL,
+  `usuario_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`producto_id`, `codigo`, `nombre`, `precio`, `stock`, `foto`, `categoria_id`, `usuario_id`) VALUES
+(3, '00005', 'Playera Levis', '250', 20, '', 5, 4),
+(4, '000000005', 'laptop hp', '5000', 10, '', 6, 4),
+(11, '000000000231', 'Poco F3 PRO', '5000', 10, 'Poco_F3_PRO_61.png', 8, 9);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `usuario_id` int NOT NULL,
+  `nombre` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `apellido` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `usuario` varchar(30) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
+  `email` varchar(30) COLLATE utf8mb4_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`usuario_id`, `nombre`, `apellido`, `usuario`, `password`, `email`) VALUES
+(4, 'Kikisito', 'Bon', 'kikisito', '$2y$10$6nGnuOh1K.urOR1jk.Kxz.TcIj2CRWLx843Qy1e65h4mYcXBdHjlG', 'kikisito@pet.com'),
+(5, 'Elliot César', 'Gandarilla', 'elliot', '$2y$10$V0FgM3/QzuwUkAve/ulY2.MD80wkKhHKw48zBz39a6auVfQi9D2iy', 'elliot@gmail.com'),
+(6, 'nano', 'banano', 'nano', '$2y$10$CMSiScvvFSoMm2449031nuR2fC.6MwdRJAn85C.BsC.k1os1DD4oe', 'nano@pet.com'),
+(9, 'Beatriz', 'Perez', 'betty', '$2y$10$YCPqE8496ZQmhiMeV0DDP.plRzXEIJLJu42MRNfSlku.5o784aZXC', 'mareli2618@hotmail.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`categoria_id`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`producto_id`),
+  ADD KEY `categoria_id` (`categoria_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`usuario_id`),
+  ADD UNIQUE KEY `usuario` (`usuario`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `categoria_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `producto_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `usuario_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`categoria_id`),
+  ADD CONSTRAINT `productos_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`usuario_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
